ee9ddbfae98fe3618af48cc2646285b958f133f1
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-11-26 23:24:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" flex justify-center items-center h-screen">
+      <button className=" bg-green-400 rounded-lg py-1 px-3">tailwind</button>
+    </div>
+  );
+}
+
+export default App;
